70fe8486b917e9bb7d262a23fc60109d2d7b20c1
iftikharkhan90 <iffirana708@gmail.com>
2025-04-09 13:20:58 +0500

    try to fix the  reload page problem

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
+function copyRedirectsPlugin() {
+  return {
+    name: 'copy-redirects',
+    closeBundle() {
+      copyFileSync('_redirects', resolve(__dirname, 'dist/_redirects'))
+    }
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), copyRedirectsPlugin()],
 })
