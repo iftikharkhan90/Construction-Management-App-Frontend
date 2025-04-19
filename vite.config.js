6f2c514b41e1113c6eeb8c8e44aa538b99195083
iftikharkhan90 <iffirana708@gmail.com>
2025-04-20 01:37:30 +0500

    itemName error solved

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'fs'
-import { resolve } from 'path'
-
-function copyRedirectsPlugin() {
-  return {
-    name: 'copy-redirects',
-    closeBundle() {
-      copyFileSync('_redirects', resolve(__dirname, 'dist/_redirects'))
-    }
-  }
-}
-
-export default defineConfig({
-  plugins: [react(), copyRedirectsPlugin()],
-})
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
+import { resolve } from 'path'
+function copyRedirectsPlugin() {
+  return {
+    name: 'copy-redirects',
+    closeBundle() {
+      copyFileSync('_redirects', resolve(__dirname, 'dist/_redirects'))
+    }
+  }
+}
+
+export default defineConfig({
+  plugins: [react(), copyRedirectsPlugin()],
+});
